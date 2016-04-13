11a560258122ead155dc83955c0e949f0ced2363
zhenglee <18368053@qq.com>
2016-04-13 13:26:35 +0800

    add style

--- Favor/src/main/java/com/zhenglee/langfangfavor/android/main/MainActivity.java
+++ Favor/src/main/java/com/zhenglee/langfangfavor/android/main/MainActivity.java
@@ -3,11 +3,8 @@ package com.zhenglee.langfangfavor.android.main;
 import android.content.Intent;
 import android.os.Bundle;
 
-import com.zhenglee.framework.res.annotation.LayoutResource;
 import com.zhenglee.framework.ui.BaseActivity;
-import com.zhenglee.langfangfavor.R;
 
-@LayoutResource(R.layout.activity_main)
 public class MainActivity extends BaseActivity {
 
     @Override
